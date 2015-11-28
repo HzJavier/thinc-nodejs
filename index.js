fabd164965b134049c54e9aa3451871e5cf69203
HzJavier <javier.hernandez@wizeservices.com>
2015-11-28 13:34:57 -0600

    Implement routes with express
    Implement get, post and put for
    /messages

--- index.js
+++ index.js
@@ -1,8 +1,37 @@
-var server = require('./server');
+var express = require('express');
+var app = express();
+var PORT = 8080;
 
-var config = {
-  port: 8080
-};
+// ROUTES
+var messages = [
+  'Message 0',
+  'Message 1',
+  'Message 2',
+  'Message 3'
+];
 
-server.config(config);
-server.start();
+app.get('/', function (req, res) {
+  res.send('Facebook messenger killer');
+});
+
+
+app.route('/messages')
+  .get(function (req, res) {
+    res.send(messages);
+  })
+  .post(function (req, res) {
+    var newMessage = 'Message ' + messages.length;
+    messages.push(newMessage);
+    res.send(newMessage);
+  });
+
+app.route('/messages/:id')
+  .put(function (req, res) {
+    var index = req.params.id;
+    var newMessage = req.query.message;
+    messages[index] = newMessage;
+    res.send(messages[index]);
+  });
+
+app.listen(PORT);
+console.log('Listening on ' + PORT);
