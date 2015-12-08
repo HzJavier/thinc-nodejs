09866429145dc3197d7771561496f82b43505c8b
HzJavier <javier.hernandez@wizeservices.com>
2015-12-07 20:43:38 -0600

    Implement firebase calls for post and get messages

--- routes/messages.js
+++ routes/messages.js
@@ -1,29 +1,49 @@
 var express = require('express');
 var router = express.Router();
+var bodyParser = require('body-parser');
+var Firebase = require('firebase');
+var firebaseRef = new Firebase("https://thinc-chat.firebaseio.com/messages");
+var MSG_LIMIT = 500;
+var TIMEOUT_MS = 100;
 
-// ROUTES
-var messages = [
-  'Message 0',
-  'Message 1',
-  'Message 2',
-  'Message 3'
-];
+router.use(bodyParser.json());
 
 router.get('/', function (req, res) {
-  res.send(messages);
+  var messages = [];
+  
+  var timeoutId = setTimeout(function () {
+    firebaseRef.off('child_added');
+    res.send(messages);
+  }, TIMEOUT_MS);
+
+  firebaseRef
+    .limitToLast(MSG_LIMIT)
+    .on('child_added', function (snapshot) {
+      messages.push(snapshot.val());
+
+      clearTimeout(timeoutId);
+
+      if (messages.length === MSG_LIMIT) {
+        firebaseRef.off('child_added');
+        res.send(messages);
+      } else {
+        timeoutId = setTimeout(function () {
+          firebaseRef.off('child_added');
+          res.send(messages); 
+        }, TIMEOUT_MS);
+      }
+
+    });
+
 });
 
 router.post('/', function (req, res) {
-  var newMessage = 'Message ' + messages.length;
-  messages.push(newMessage);
-  res.send(newMessage);
-});
+  var message = req.body || {};
+  message.timestamp = Date.now();
 
-router.put('/:id', function (req, res) {
-  var index = req.params.id;
-  var newMessage = req.query.message;
-  messages[index] = newMessage;
-  res.send(messages[index]);
+  firebaseRef.push(message);
+
+  res.send('success');
 });
 
 module.exports = router;
